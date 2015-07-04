5b80a1f5ea567434d04e7e51e0ff16454db8f34b
Kate Ward <kate.ward@forestent.com>
2015-07-04 21:07:58 +0200

    allow for UI settling on MouseLeftClick()

--- hid.go
+++ hid.go
@@ -23,7 +23,7 @@ func (v *Venue) KeyPress(key uint32) error {
 // MouseMove moves the mouse.
 func (v *Venue) MouseMove(p image.Point) {
 	v.conn.PointerEvent(vnc.ButtonNone, uint16(p.X), uint16(p.Y))
-	time.Sleep(uiSettle) // Give mouse some time to "settle".
+	time.Sleep(uiSettle) // Give UI some time to "settle".
 }
 
 // MouseLeftClick moves the mouse to a position and left clicks.
@@ -31,6 +31,7 @@ func (v *Venue) MouseLeftClick(p image.Point) {
 	v.MouseMove(p)
 	v.conn.PointerEvent(vnc.ButtonLeft, uint16(p.X), uint16(p.Y))
 	v.conn.PointerEvent(vnc.ButtonNone, uint16(p.X), uint16(p.Y))
+	time.Sleep(uiSettle) // Give UI some time to "settle".
 }
 
 // MouseDrag moves the mouse, clicks, and drags to a new position.
@@ -39,6 +40,6 @@ func (v *Venue) MouseDrag(p, d image.Point) {
 	v.conn.PointerEvent(vnc.ButtonLeft, uint16(p.X), uint16(p.Y))
 	p = p.Add(d) // Add delta.
 	v.conn.PointerEvent(vnc.ButtonLeft, uint16(p.X), uint16(p.Y))
-	time.Sleep(uiSettle) // Give mouse some time to "settle".
+	time.Sleep(uiSettle) // Give UI some time to "settle".
 	v.conn.PointerEvent(vnc.ButtonNone, uint16(p.X), uint16(p.Y))
 }
