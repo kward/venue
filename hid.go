f458bf9445ca9ddb2e0837aa652622d01f0d4995
Kate Ward <kate.ward@forestent.com>
2015-07-09 00:17:27 +0200

    removed UI settling pauses as I moved them to go-vnc

--- hid.go
+++ hid.go
@@ -2,14 +2,8 @@ package venue
 
 import (
 	"image"
-	"time"
 
 	vnc "github.com/kward/go-vnc"
-)
-
-const (
-	// Time to allow UI to settle.
-	uiSettle = 10 * time.Millisecond
 )
 
 // KeyPress presses a key on the VENUE console.
@@ -17,27 +11,22 @@ func (v *Venue) KeyPress(key uint32) error {
 	if err := v.conn.KeyEvent(key, true); err != nil {
 		return err
 	}
-	time.Sleep(uiSettle)
 	if err := v.conn.KeyEvent(key, false); err != nil {
 		return err
 	}
-	time.Sleep(uiSettle)
 	return nil
 }
 
 // MouseMove moves the mouse.
 func (v *Venue) MouseMove(p image.Point) {
 	v.conn.PointerEvent(vnc.ButtonNone, uint16(p.X), uint16(p.Y))
-	time.Sleep(uiSettle)
 }
 
 // MouseLeftClick moves the mouse to a position and left clicks.
 func (v *Venue) MouseLeftClick(p image.Point) {
 	v.MouseMove(p)
 	v.conn.PointerEvent(vnc.ButtonLeft, uint16(p.X), uint16(p.Y))
-	time.Sleep(uiSettle)
 	v.conn.PointerEvent(vnc.ButtonNone, uint16(p.X), uint16(p.Y))
-	time.Sleep(uiSettle)
 }
 
 // MouseDrag moves the mouse, clicks, and drags to a new position.
@@ -46,6 +35,5 @@ func (v *Venue) MouseDrag(p, d image.Point) {
 	v.conn.PointerEvent(vnc.ButtonLeft, uint16(p.X), uint16(p.Y))
 	p = p.Add(d) // Add delta.
 	v.conn.PointerEvent(vnc.ButtonLeft, uint16(p.X), uint16(p.Y))
-	time.Sleep(uiSettle)
 	v.conn.PointerEvent(vnc.ButtonNone, uint16(p.X), uint16(p.Y))
 }
