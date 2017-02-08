d6de568fa93c15a08be16a732b3329b1549f5be9
Kate Ward <kate.ward@forestent.com>
2017-02-08 23:05:50 +0100

    fleshed out workflow unit tests

--- vnc/workflow.go
+++ vnc/workflow.go
@@ -26,18 +26,36 @@ import (
 type Event struct {
 	desc string           // Description of the event.
 	msg  messages.Message // Type of event.
-	data []interface{}    // Supporting data.
+	data interface{}      // Event data.
+}
+
+type keyEvent struct {
+	key  keys.Key
+	down bool
+}
+
+type pointerEvent struct {
+	button buttons.Button
+	x, y   uint16
+}
+
+type sleepEvent struct {
+	d time.Duration
 }
 
 // Workflow holds a client connection to the VNC server, and a list of events.
 type Workflow struct {
-	conn   ClientConn
-	events []*Event
+	conn    ClientConn
+	events  []*Event
+	sleeper Sleeper
 }
 
 // NewWorkflow returns a new workflow object.
 func NewWorkflow(conn ClientConn) *Workflow {
-	return &Workflow{conn: conn}
+	return &Workflow{
+		conn:    conn,
+		sleeper: newWorkflowSleeper(),
+	}
 }
 
 func (wf *Workflow) enqueue(e *Event) {
@@ -61,18 +79,19 @@ func (wf *Workflow) Execute() error {
 		}
 		switch event.msg {
 		case messages.KeyEvent:
-			key, down := event.data[0].(keys.Key), event.data[1].(bool)
-			if err := wf.conn.KeyEvent(key, down); err != nil {
+			e := event.data.(keyEvent)
+			if err := wf.conn.KeyEvent(e.key, e.down); err != nil {
 				return err
 			}
 		case messages.PointerEvent:
-			button, x, y := event.data[0].(buttons.Button), event.data[1].(uint16), event.data[2].(uint16)
-			if err := wf.conn.PointerEvent(button, x, y); err != nil {
+			e := event.data.(pointerEvent)
+			if err := wf.conn.PointerEvent(e.button, e.x, e.y); err != nil {
 				return err
 			}
 		case messages.Sleep:
-			d := event.data[0].(time.Duration)
-			time.Sleep(d)
+			e := event.data.(sleepEvent)
+			wf.sleeper.Sleep(e.d)
+			time.Sleep(e.d)
 		}
 	}
 	return nil
@@ -83,11 +102,11 @@ func (wf *Workflow) KeyPress(key keys.Key) {
 	wf.enqueue(&Event{
 		fmt.Sprintf("press %s", key),
 		messages.KeyEvent,
-		[]interface{}{key, vnclib.PressKey}})
+		keyEvent{key, vnclib.PressKey}})
 	wf.enqueue(&Event{
 		fmt.Sprintf("release %s", key),
 		messages.KeyEvent,
-		[]interface{}{key, vnclib.ReleaseKey}})
+		keyEvent{key, vnclib.ReleaseKey}})
 }
 
 // MouseMove moves the mouse.
@@ -95,7 +114,7 @@ func (wf *Workflow) MouseMove(p image.Point) {
 	wf.enqueue(&Event{
 		fmt.Sprintf("move mouse to %s", p),
 		messages.PointerEvent,
-		[]interface{}{buttons.None, uint16(p.X), uint16(p.Y)}})
+		pointerEvent{buttons.None, uint16(p.X), uint16(p.Y)}})
 }
 
 // MouseClick moves the mouse to a position and left clicks.
@@ -104,11 +123,11 @@ func (wf *Workflow) MouseClick(b buttons.Button, p image.Point) {
 	wf.enqueue(&Event{
 		fmt.Sprintf("%s button click at %s", b, p),
 		messages.PointerEvent,
-		[]interface{}{b, uint16(p.X), uint16(p.Y)}})
+		pointerEvent{b, uint16(p.X), uint16(p.Y)}})
 	wf.enqueue(&Event{
 		fmt.Sprintf("%s button release", b),
 		messages.PointerEvent,
-		[]interface{}{buttons.None, uint16(p.X), uint16(p.Y)}})
+		pointerEvent{buttons.None, uint16(p.X), uint16(p.Y)}})
 }
 
 // MouseDrag moves the mouse, clicks, and drags to a new position.
@@ -117,16 +136,16 @@ func (wf *Workflow) MouseDrag(p, d image.Point) {
 	wf.enqueue(&Event{
 		fmt.Sprintf("%s button click at %s", buttons.Left, p),
 		messages.PointerEvent,
-		[]interface{}{buttons.Left, uint16(p.X), uint16(p.Y)}})
+		pointerEvent{buttons.Left, uint16(p.X), uint16(p.Y)}})
 	p = p.Add(d) // Add delta.
 	wf.enqueue(&Event{
 		fmt.Sprintf("mouse drag to %s", p),
 		messages.PointerEvent,
-		[]interface{}{buttons.Left, uint16(p.X), uint16(p.Y)}})
+		pointerEvent{buttons.Left, uint16(p.X), uint16(p.Y)}})
 	wf.enqueue(&Event{
 		fmt.Sprintf("%s button release", buttons.Left),
 		messages.PointerEvent,
-		[]interface{}{buttons.None, uint16(p.X), uint16(p.Y)}})
+		pointerEvent{buttons.None, uint16(p.X), uint16(p.Y)}})
 }
 
 // Sleep the workflow for at least the duration d.
@@ -134,5 +153,15 @@ func (wf *Workflow) Sleep(d time.Duration) {
 	wf.enqueue(&Event{
 		fmt.Sprintf("sleep for %s", d),
 		messages.Sleep,
-		[]interface{}{d}})
+		sleepEvent{d}})
 }
+
+type Sleeper interface {
+	Sleep(d time.Duration)
+}
+
+type workflowSleeper struct{}
+
+func newWorkflowSleeper() Sleeper { return &workflowSleeper{} }
+
+func (s *workflowSleeper) Sleep(d time.Duration) { time.Sleep(d) }
--- vnc/workflow_test.go
+++ vnc/workflow_test.go
@@ -1,8 +1,10 @@
 package vnc
 
 import (
+	"image"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/kward/go-vnc/buttons"
 	"github.com/kward/go-vnc/keys"
@@ -25,21 +27,78 @@ func TestKeyPress(t *testing.T) {
 	}
 }
 
+func TestMouseMove(t *testing.T) {
+	conn := NewMockConn()
+	wf := NewWorkflow(conn)
+	wf.MouseMove(image.Point{100, 200})
+	wf.Execute()
+
+	events := Events{
+		pointerEvent{buttons.None, 100, 200},
+	}
+	if got, want := conn.(*mockConn).events, events; !reflect.DeepEqual(got, want) {
+		t.Errorf("unexpected events; < %v > != < %v >", got, want)
+	}
+}
+
+func TestMouseClick(t *testing.T) {
+	conn := NewMockConn()
+	wf := NewWorkflow(conn)
+	wf.MouseClick(buttons.Left, image.Point{100, 200})
+	wf.Execute()
+
+	events := Events{
+		pointerEvent{buttons.None, 100, 200},
+		pointerEvent{buttons.Left, 100, 200},
+		pointerEvent{buttons.None, 100, 200},
+	}
+	if got, want := conn.(*mockConn).events, events; !reflect.DeepEqual(got, want) {
+		t.Errorf("unexpected events; < %v > != < %v >", got, want)
+	}
+}
+
+func TestMouseDrag(t *testing.T) {
+	conn := NewMockConn()
+	wf := NewWorkflow(conn)
+	wf.MouseDrag(image.Point{100, 200}, image.Point{10, -20})
+	wf.Execute()
+
+	events := Events{
+		pointerEvent{buttons.None, 100, 200},
+		pointerEvent{buttons.Left, 100, 200},
+		pointerEvent{buttons.Left, 110, 180},
+		pointerEvent{buttons.None, 110, 180},
+	}
+	if got, want := conn.(*mockConn).events, events; !reflect.DeepEqual(got, want) {
+		t.Errorf("unexpected events; < %v > != < %v >", got, want)
+	}
+}
+
+func TestSleep(t *testing.T) {
+	conn := NewMockConn()
+	wf := NewWorkflow(conn)
+	wf.sleeper = newMockSleeper()
+	wf.Sleep(1 * time.Second)
+	wf.Execute()
+
+	events := Events{
+		sleepEvent{1 * time.Second},
+	}
+	if got, want := wf.sleeper.(*mockSleeper).events, events; !reflect.DeepEqual(got, want) {
+		t.Errorf("unexpected events; < %v > != < %v >", got, want)
+	}
+}
+
+//-----------------------------------------------------------------------------
+// mockConn implements the ClientConn interface.
 type mockConn struct {
 	events Events
 }
 
-func NewMockConn() ClientConn {
-	return &mockConn{}
-}
+func NewMockConn() ClientConn { return &mockConn{} }
 
 func (c *mockConn) FramebufferHeight() uint16 { return 0 }
 func (c *mockConn) FramebufferWidth() uint16  { return 0 }
-
-type keyEvent struct {
-	key  keys.Key
-	down bool
-}
 
 func (c *mockConn) KeyEvent(key keys.Key, down bool) error {
 	c.events = append(c.events, keyEvent{key, down})
@@ -47,6 +106,7 @@ func (c *mockConn) KeyEvent(key keys.Key, down bool) error {
 }
 
 func (c *mockConn) PointerEvent(button buttons.Button, x, y uint16) error {
+	c.events = append(c.events, pointerEvent{button, x, y})
 	return nil
 }
 
@@ -54,3 +114,13 @@ func (c *mockConn) Close() error                                                
 func (c *mockConn) DebugMetrics()                                               {}
 func (c *mockConn) FramebufferUpdateRequest(int uint8, x, y, w, h uint16) error { return nil }
 func (c *mockConn) ListenAndHandle() error                                      { return nil }
+
+//-----------------------------------------------------------------------------
+// mockSleeper implements the Sleeper interface.
+type mockSleeper struct {
+	events Events
+}
+
+func newMockSleeper() Sleeper { return &mockSleeper{} }
+
+func (s *mockSleeper) Sleep(d time.Duration) { s.events = append(s.events, sleepEvent{d}) }
