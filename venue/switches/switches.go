bd26a7d4ff51ec16e9ad10178baaf50f3ee1d635
Kate Ward <kate.ward@forestent.com>
2017-02-09 00:02:20 +0100

    fleshed out the comments, and added generated strings

--- venue/switches/switches.go
+++ venue/switches/switches.go
@@ -1,22 +1,39 @@
+// Packages switches defines different sizes, kinds, and states of switches.
 package switches
 
-type SwitchSize int
+// Size defines size of a switch.
+type Size int
 
-// Switch size examples:
-// - Tiny: Channel solo or mute on bank (13x13 px)
-// - Small: Encoder ON (18x14 px)
-// - Medium: 48V, Pad, Guess (26x16 px)
-// - Large: Channel solo or mute (32x18)
-const ( // Switch size.
-	Tiny SwitchSize = iota
+//go:generate stringer -type=Size
+
+const (
+	// Tiny: Channel solo or mute on bank (13x13 px)
+	Tiny Size = iota
+	// Small: Encoder ON (18x14 px)
 	Small
+	// Medium: 48V, Pad, Guess (26x16 px)
 	Medium
+	// Large: Channel solo or mute (32x18)
 	Large
 )
 
-type SwitchKind int
+// Kind defines the kind of a switch.
+type Kind int
+
+//go:generate stringer -type=Kind
 
 const (
-	Toggle SwitchKind = iota
+	Toggle Kind = iota
 	PushButton
+)
+
+// Toggle switch states.
+const (
+	// Enabled indicates the switch was pressed. On the console, this is
+	// indicated by an LED on the switch, and on the UI, an equivalent LED-like
+	// indicator.
+	Enabled = true
+
+	// Disabled indicates the switch is not active.
+	Disabled = false
 )
